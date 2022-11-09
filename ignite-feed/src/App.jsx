767040f7dd95a15e714de1661dffbdbd7cd57ac7
edgardfn <edgardfinotti@yahoo.com.br>
2022-11-09 16:48:23 -0300

    criado projeto inicial react com vite e criado estrutura basica do header.

---
+++ ignite-feed/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Header } from './components/Header'
+
+import './styles.css'
+
+export function App() {
+  
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
