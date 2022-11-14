3c99ca3a0c1f50301ad4bcb1bc225dadb66aa36e
edgardfn <edgardfinotti@yahoo.com.br>
2022-11-14 16:46:51 -0300

    criado componente Post e sua estrutura html.

---
+++ ignite-feed/src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img src="https://github.com/edgardfn.png" className={styles.avatar} />
+                    <div className={styles.authorInfo}>
+                        <strong>Edgard Finotti</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='14 de Novembro às 16:00' dateTime="2022-11-14 08:13:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p> Fala galeraa 👋 </p>
+                <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
+                <p>👉 <a href=" ">jane.design/doctorcare</a> </p>
+                <p> <a href="">#novoprojeto #nlw #rocketseat</a> </p>
+            </div>
+        </article>
+    )
+}
