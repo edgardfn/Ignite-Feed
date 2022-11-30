d22a41dd042e65e204d420b0f81e9c6f2cf31327
edgardfn <edgardfinotti@yahoo.com.br>
2022-11-30 17:54:12 -0300

    criado componente Avatar.

--- ignite-feed/src/components/Sidebar.jsx
+++ ignite-feed/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import {PencilLine} from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,9 @@ export function Sidebar () {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/edgardfn.png" />
+                <Avatar 
+                    src='https://github.com/edgardfn.png'
+                />
 
                 <strong>Edgard Finotti Neto</strong>
                 <span>Web Developer</span>
