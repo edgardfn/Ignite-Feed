11531f060a2ed86db9db8320c93ccd907a999ef5
edgardfn <edgardfinotti@yahoo.com.br>
2022-11-30 15:00:32 -0300

    criacao da estrutura dos comentarios.

---
+++ ignite-feed/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles  from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/edgardfn.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Edgard Finotti Neto</strong>
+                            <time title='14 de Novembro às 16:00' dateTime="2022-11-14 08:13:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
