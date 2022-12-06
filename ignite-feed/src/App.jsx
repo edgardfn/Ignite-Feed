b2aa75d5653298e51aac8bd653f2b9b2168097bb
edgardfn <edgardfinotti@yahoo.com.br>
2022-12-06 15:09:44 -0300

    alterando dados do post para receber dados dinamicos e estilizando a exibição de data dos posts.

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,14 +1,43 @@
-import { Header } from './components/Header'
-import { Post } from './components/Post'
-import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
-import styles from './App.module.css'
+import styles from './App.module.css';
 
-import './global.css'
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/edgardfn.png',
+      name: 'Edgard Finotti Neto',
+      role: 'Desenvolvedor Front-End'
+    },
+    content: [
+      {type:'paragraph', content:'Fala galeraa 👋'},
+      {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type:'link', content:'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-04 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Deigo Fernandes',
+      role: 'CTO @Rocketseat',
+    },
+    content: [
+      {type:'paragraph', content:'Fala galeraa 👋'},
+      {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type:'link', content:'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-06 10:00:00'),
+  },
+]
 
 export function App() {
-  
-
   return (
     <div>
       <Header />
@@ -16,14 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Edgard Finotti Neto"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere ad eius, enim facilis et ducimus iste libero exercitationem sint, est dolore minus explicabo, dicta animi. Magnam quae dolor quasi deleniti!"
-          />
-          <Post 
-            author="Melina Losnak Moreno"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere ad eius, enim facilis et ducimus iste libero exercitationem sint, est dolore minus explicabo, dicta animi. Magnam quae dolor quasi deleniti!"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
