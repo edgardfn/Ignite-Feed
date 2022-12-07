82edae61f9503ce1ed827151169debe02824086e
edgardfn <edgardfinotti@yahoo.com.br>
2022-12-07 15:10:39 -0300

    criado validação de formulario de comentario.

--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -36,7 +36,12 @@ export function Post({author, publishedAt, content, id}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
     }
 
     function deleteComment(commentToDelete) {
@@ -47,6 +52,8 @@ export function Post({author, publishedAt, content, id}) {
         
         setComments(commentsWithoutDeleteOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
 
     return (
@@ -85,10 +92,17 @@ export function Post({author, publishedAt, content, id}) {
                     placeholder='Deixe um comentário'
                     onChange={handleNewCommentChange}
                     value={newCommentText}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button 
+                        type='submit' 
+                        disabled={isNewCommentEmpty}
+                    >
+                        Publicar
+                    </button>
                 </footer>
                 
             </form>
